121a486629cc834c10d2a6b9eebe161144c6eda8
Nikhil Anand <nikhilanand@granular.ag>
2021-02-07 13:07:14 -0600

    Incremental

--- _data/site.js
+++ _data/site.js
@@ -1,9 +1,13 @@
-const REMOTE_URI = "https://public.nikhil.io/stats";
-
 module.exports = {
   name: "Statistics Notes",
   description:
     "My statistics notes and bookmarks for my classes at Georgia Tech",
   locale: "en-US",
-  url: process.env.CI ? REMOTE_URI : "",
+
+  /**
+   * Because I have this deployed at
+   *
+   * https://public.nikhil.io/stats
+   */
+  pathPrefix: "/stats/",
 };
