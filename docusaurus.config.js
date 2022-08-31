e85dbf9ad3b4b46e34c13856bcf9b77d5046288a
Nikhil Anand <mail@nikhil.io>
2022-08-31 17:57:54 -0500

    URI Adjust

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: "/stats",
+          routeBasePath: "/",
           remarkPlugins: [math],
           rehypePlugins: [katex],
           sidebarPath: require.resolve("./sidebars.js"),
